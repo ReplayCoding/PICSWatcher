6bcf928df402e58cd20eefdcec22bcd3f12d903e
ReplayCoding <replaycoding@gmail.com>
2023-08-24 11:06:06 -0400

    discard empty changes
    some changelists don't change any public facing info, so they appear
    empty.

--- PICSChanges.cs
+++ PICSChanges.cs
@@ -4,10 +4,22 @@ using Dapper;
 
 using SteamKit2;
 
+using System.Data;
+using System.Linq;
+
 class PICSChanges
 {
     private uint LastChangeNumber = 0;
     private uint TickerHash = 0;
+
+    private class DepotManifestPair
+    {
+        public uint DepotID;
+        public ulong ManifestID;
+
+        public DepotManifestPair(uint depotID, ulong manifestID)
+            => (DepotID, ManifestID) = (depotID, manifestID);
+    }
 
     public PICSChanges(CallbackManager manager)
     {
@@ -49,6 +61,71 @@ class PICSChanges
         };
     }
 
+    private async Task AddChangesToDB(InfoFetcher.AppInfo appInfo, IDbConnection db, IDbTransaction transaction)
+    {
+        foreach (var depot in appInfo.Depots)
+        {
+            await db.ExecuteAsync(@"
+                            INSERT INTO `DepotVersions`
+                                (`ChangeID`, `AppID`, `DepotID`, `ManifestID`)
+                                VALUES (@ChangeID, @AppID, @DepotID, @ManifestID)
+                                ON DUPLICATE KEY UPDATE `ChangeID`=`ChangeID`;",
+            new
+            {
+                ChangeID = appInfo.ChangeId,
+                AppID = appInfo.AppId,
+                DepotID = depot.DepotId,
+                ManifestID = depot.ManifestId,
+            }, transaction);
+        }
+
+        await db.ExecuteAsync(@"
+                            INSERT INTO `BuildInfo`
+                                (`ChangeID`, `Branch`, `BuildID`, `TimeUpdated`)
+                                VALUES (@ChangeID, @Branch, @BuildID, @TimeUpdated)
+                                ON DUPLICATE KEY UPDATE `ChangeID`=`ChangeID`;",
+        new
+        {
+            ChangeID = appInfo.ChangeId,
+            Branch = Program.Config.Branch,
+            BuildID = appInfo.BuildID,
+            TimeUpdated = appInfo.TimeUpdated
+        }, transaction);
+    }
+
+    public async Task<bool> ChangeDiffersFromPrev(InfoFetcher.AppInfo appInfo, uint lastChangeNumber, IDbConnection db, IDbTransaction transaction)
+    {
+        Dictionary<uint, DepotManifestPair> prevDepots = (await db.QueryAsync<DepotManifestPair>(
+                @"SELECT `DepotID`, `ManifestID` FROM `DepotVersions`
+                    WHERE `ChangeID` = @ChangeID AND `AppID` = @AppID", new
+                {
+                    ChangeID = lastChangeNumber,
+                    AppID = appInfo.AppId,
+                }, transaction: transaction)).ToDictionary(depot => depot.DepotID);
+
+        Dictionary<uint, DepotManifestPair> currentDepots =
+            appInfo.Depots
+            .Select(depot => new DepotManifestPair(depot.DepotId, depot.ManifestId))
+            .ToDictionary(depot => depot.DepotID);
+
+        // If the number of depots differs, a depot was added or removed.
+        if (prevDepots.Count != currentDepots.Count)
+            return true;
+
+        foreach (uint depotId in currentDepots.Keys)
+        {
+            // prev doesn't contain a depot, so it must have been removed.
+            if (!prevDepots.ContainsKey(depotId))
+                return true;
+
+            // prev contains depot, but the manifest differs.
+            if (prevDepots[depotId].ManifestID != currentDepots[depotId].ManifestID)
+                return true;
+        }
+
+        return false;
+    }
+
     public async void OnPICSChanges(SteamApps.PICSChangesCallback cb)
     {
         bool needsUpdate = false;
@@ -81,42 +158,18 @@ class PICSChanges
             var appInfo = await InfoFetcher.FetchAppInfo(Program.Config.AppToWatch, changeNumber);
             if (appInfo != null)
             {
-
-                foreach (var depot in appInfo.Depots)
-                {
-                    await db.ExecuteAsync(@"
-                                INSERT INTO `DepotVersions`
-                                    (`ChangeID`, `AppID`, `DepotID`, `ManifestID`)
-                                    VALUES (@ChangeID, @AppID, @DepotID, @ManifestID)
-                                    ON DUPLICATE KEY UPDATE `ChangeID`=`ChangeID`;",
-                    new
-                    {
-                        ChangeID = appInfo.ChangeId,
-                        AppID = appInfo.AppId,
-                        DepotID = depot.DepotId,
-                        ManifestID = depot.ManifestId,
-                    }, transaction);
-                }
-
-                await db.ExecuteAsync(@"
-                                INSERT INTO `BuildInfo`
-                                    (`ChangeID`, `Branch`, `BuildID`, `TimeUpdated`)
-                                    VALUES (@ChangeID, @Branch, @BuildID, @TimeUpdated)
-                                    ON DUPLICATE KEY UPDATE `ChangeID`=`ChangeID`;",
-                new
-                {
-                    ChangeID = appInfo.ChangeId,
-                    Branch = Program.Config.Branch,
-                    BuildID = appInfo.BuildID,
-                    TimeUpdated = appInfo.TimeUpdated
-                }, transaction);
-
+                needsUpdate = await ChangeDiffersFromPrev(appInfo, LastChangeNumber, db, transaction);
+                if (needsUpdate)
+                    await AddChangesToDB(appInfo, db, transaction);
+                else
+                    Console.WriteLine("Ignoring empty change: {0}", appInfo.ChangeId);
             }
             else
             {
-                Console.WriteLine("Couldn't get appinfo!");
+                throw new Exception("Couldn't get appinfo!");
             }
         }
+
 
         LastChangeNumber = cb.CurrentChangeNumber;
         await LocalConfig.SetAsync("lastSeenChangeNumber", LastChangeNumber.ToString());
